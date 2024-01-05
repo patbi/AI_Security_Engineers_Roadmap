0f866bf020a8933f1f8cf29ad4d07238a9d1854e
patbi <biyagapatrick@gmail.com>
2024-01-05 09:59:30 +0100

    Day 2/100 Module one

---
+++ Module1_Python_Basics/Module1_Python_Basics.py
@@ -0,0 +1,29 @@
+"""
+1 - Variables in Python
+"""
+a = 9
+b = 16
+print(a)
+print(12)
+print(b)
+print(a * b * 5)
+fname = 'Data Engineer -'
+lname = 'ML/AI'
+print(fname+' '+lname)
+print(a,b,fname,lname)
+
+"""
+2 - Strings and Variables in Python 
+"""
+
+name = "Patbi"
+ip = 12
+print("His name is",name,"and His ip is",ip)
+
+x = "2"
+y = '7'
+print(x + y)
+print(int(x) + int(y))
+
+z = "this is all about string"
+print(z)
