5df983f8886b3e92e5583e352ecc5f3560f5b545
patbi <biyagapatrick@gmail.com>
2024-01-16 06:45:24 +0100

    Day-13/100 Learning Module3

---
+++ Module 3_Package_Managers/Module 3_Package_Managers.py
@@ -0,0 +1,12 @@
+<!-- ACKNOWLEDGMENTS -->
+## Acknowledgments:
+
+Use this space to list resources you find helpful and would like to give credit to. I've included a few of my favorites to kick things off!
+
+
+
+* [Pip Python Tutorial for Package Management](https://www.datacamp.com/tutorial/pip-python-package-manager)
+* [Pip documentation](https://pip.pypa.io/en/stable/getting-started/)
+* [Conda Documentation](https://docs.conda.io/en/latest/)
+* [Getting started with conda](https://conda.io/projects/conda/en/latest/user-guide/getting-started.html#managing-python)
+* [Miniconda](https://docs.conda.io/projects/miniconda/en/latest/)
