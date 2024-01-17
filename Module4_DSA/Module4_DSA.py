3b8fff67391792639f555eb6e072a30eebc4b132
patbi <biyagapatrick@gmail.com>
2024-01-17 20:25:14 +0100

    Day-14/100 Learning Module4

---
+++ Module4_DSA/Module4_DSA.py
@@ -0,0 +1,29 @@
+"""
+* DATA STRUCTURES:
+
+	Why we need to use data structure ?
+
+	- Data structures: to store in an efficient way
+	
+	Why to use data structures ?
+
+	- We often have the intuition --> if we want to make an algorithm fast,
+		we have to optimize it
+	- Avoid nested for loops
+	- Make every calculation as fast as possible
+	- But algorithms can be boosted up by proper data structures
+	- Data structures make sure the running time will be better
+	- Dijkstra algorithm
+	- Without a proper data structure (heap / priority queue ) the running
+		time would be quadratic // O(N²) 
+	- Priority queue approach makes sure the running time will be far
+		better // O(N*logN)
+"""
+
+
+
+"""
+* Difference between Data structures and abstract data types (ADT)
+
+
+"""
