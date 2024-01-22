d3af8a82fa8d2ff729a26a26b0005297eb93b0b3
patbi <biyagapatrick@gmail.com>
2024-01-22 20:54:13 +0100

    Day-19/100 Learning Module4

--- Module4_DSA/Module4_DSA.py
+++ Module4_DSA/Module4_DSA.py
@@ -302,3 +302,49 @@ Waste space                 O(N)               0
 * Linked list insertion implementation in python
 
 """
+
+class Node(object):
+	def __init__(self, data):
+		self.data = data;
+		self.nextNode = None;
+
+
+class LinkedList(object):
+	def __init__(self):
+		self.head = None;
+		self.size = 0;
+
+	
+	#O(1)
+	def insertStart(self, data):
+		self.size = self.size + 1;
+		newNode = Node(data);
+
+		if not self.head:
+			self.head = newNode;
+		else:
+			newNode.nextNode = self.head;
+			self.head = newNode;
+
+
+	#O(1)
+	def size(self):
+		return self.size;
+
+
+	# O(N) not good
+	def size2(self):
+		actualNode = self.head;
+		size = 0;
+
+
+		while actualNode is not None:
+			size+=1;
+			actualNode = actualNode.nextNode;
+		return size;
+
+
+
+"""
+* Linked list implementation in python (travese)
+"""		
