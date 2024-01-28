533f50609bcdbd2da9247be8ce0c4339371dea67
patbi <biyagapatrick@gmail.com>
2024-01-28 20:57:42 +0100

    Day-25/100 Learning Module4

--- Module4_DSA/Module4_DSA.py
+++ Module4_DSA/Module4_DSA.py
@@ -576,8 +576,26 @@ class Stack:
 		self.stack.append(data)
 
 	def pop(self):
+		data = self.stack[-1]
+		del self.stack[-1]
+		return data
 
+	def peek(self):
+		return self.stack[-1]
 
+	def sizeStack(self):
+		return len(self.stack)
+
+stack = Stack()
+stack.push(1)
+stack.push(2)
+stack.push(3)
+print(stack.sizeStack())
+print("Popped: ", stack.pop())
+print("Popped: ", stack.pop())
+print(stack.sizeStack())
+print("Peek:", stack.peek())
+print(stack.sizeStack())
 
 
 
