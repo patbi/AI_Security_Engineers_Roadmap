9ea283ccb78aa257cc34f9d52a9d4c7b6b8f8f23
patbi <biyagapatrick@gmail.com>
2024-03-16 18:19:42 +0100

    Day-68/100 Learning Module 4

--- Module4_DSA/Module4_DSA.py
+++ Module4_DSA/Module4_DSA.py
@@ -1876,10 +1876,47 @@ bfs(vertex)
 
 
 
-# Breadth first search implementation in python
+# Breadth-first search implementation in python
 
 class Node(object):
 
+	def __init__(self, name):
+		self.name = name;
+		self.adjacencyList = [];
+		self.visited = False;
+		self.predecessor = None;
+
+class BreadthFirstSearch(object):
+
+	def bfs(self, startNode):
+
+		queue = [];
+		queue.append(startNode);
+		startNode.visited = True;
+
+		while queue:
+
+			actualNode = queue.pop(0);
+			print("%s " % actualNode.data);
+
+			for n in actualNode.adjacencyList:
+				if not n.visited:
+					n.visited = True;
+					queue.append(n);
+
+node1 = Node("A");
+node2 = Node("B");
+node3 = Node("C");
+node4 = Node("D");
+node5 = Node("E");
+
+node1.adjacencyList.append(node2);
+node2.adjacencyList.append(node3);
+node2.adjacencyList.append(node4);
+node4.adjacencyList.append(node5);
+
+bfs = BreadthFirstSearch();
+bfs.bfs(node1);
 
 
 
