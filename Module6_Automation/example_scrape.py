1f3c65f115f31681bf6843abb5494d885e37ebd1
patbi <biyagapatrick@gmail.com>
2024-04-08 22:33:17 +0100

    Day-84/100 Learning Module 6

--- Module6_Automation/example_scrape.py
+++ Module6_Automation/example_scrape.py
@@ -1,8 +1,36 @@
 from bs4 import BeautifulSoup
 import requests
 
+source = requests.get('https://open.spotify.com/').text
 
-with open('example.html') as html_file:
-	soup = BeautifulSoup(html_file, 'lxml')
+soup = BeautifulSoup(source, 'lxml')
 
-print(soup)
+print(soup.prettify())
+
+# with open('example.html') as html_file:
+# 	soup = BeautifulSoup(html_file, 'lxml')
+
+# print(soup.prettify())
+# match = soup.title.text
+# match = soup.div
+# match = soup.find('div')
+# match = soup.find('div', class_='footer')
+# print(match)
+
+# article = soup.find('div', class_='article')
+# print(article)
+
+# headline = article.h2.a.text
+# print(headline)
+
+# summary = article.p.text
+# print(summary)
+
+
+# for article in soup.find_all('div', class_='article'):
+# 	headline = article.h2.a.text
+# 	print(headline)
+
+# 	summary = article.p.text
+# 	print(summary)
+
