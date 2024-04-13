3a659c71c606c02c43ce3abfa05e1bb287f8afc6
patbi <biyagapatrick@gmail.com>
2024-04-13 23:49:15 +0100

    Day-89/100 Learning Module 6

--- Module6_Automation/example_scrape.py
+++ Module6_Automation/example_scrape.py
@@ -7,10 +7,25 @@ soup = BeautifulSoup(source, 'lxml')
 
 html = soup.find('html')
 
-# print(soup.prettify())
+print(soup.prettify())
 
-summary = html.find('div', class_='errorContainer').h1.text
-print(summary)
+# summary = html.find('div', class_='errorContainer').h1.text
+# print(summary)
+
+# vid_src = html.find('img', alt='Twitter')['src']
+# print(vid_src)
+
+# Value = soup.find_all("p", {"class": "errorButton"})
+# print(Value)
+
+# Players = soup.find_all("a", {"class": ""})
+
+# imgs = [html.find('img') for html in soup.find_all("html", {"class": ""}) if html.find('img')]
+# team = [img.get('alt') for img in imgs]
+# print(team)
+
+Team = [html.find('img').get('alt') for html in soup.find_all("html", {"class": ""}) if html.find('img')]
+print(Team)
 
 # with open('example.html') as html_file:
 # 	soup = BeautifulSoup(html_file, 'lxml')
