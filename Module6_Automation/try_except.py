ffc48db1b2b374849182174e1fe4d675aac3a1e8
patbi <biyagapatrick@gmail.com>
2024-04-19 19:32:21 +0100

    Day-93/100 Learning Module 6

---
+++ Module6_Automation/try_except.py
@@ -0,0 +1,22 @@
+
+try:
+	# f = open('testfiles.txt')
+	f = open('currupt_file.txt')
+	# if f.name == 'currupt_file':
+	# 	raise Exception
+	# var = bad_var
+# except Exception:
+# except FileNotFoundError:
+except FileNotFoundError as e:
+	# print("Sorry. This file doesn't exist")
+	print(e)
+# except Exception:
+except Exception as e:
+	# print('Sorry. Something went wrong')
+	# print(e)
+	print('Error!')
+else:
+	print(f.read())
+	f.close()
+finally:
+	print("Executing Finally...")
